00be991447be90fa3401259070c02c49c514c417
ajilantang <ajilantang@gmail.com>
2017-07-12 13:48:56 +0700

    folderStructure

---
+++ app/component/Player/index.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  Button,
+  View
+} from 'react-native';
+
+export default class PlayerScreen extends Component {
+  static navigationOptions = {
+    title: 'Music Player',
+  };
+  render() {
+    return (
+      <View>
+        
+      </View>
+    );
+  }
+}
