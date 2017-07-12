d90fe445163d5f0f7594233f414ad62b5d3f2b38
ajilantang <ajilantang@gmail.com>
2017-07-12 18:55:57 +0700

    scheduling

--- app/component/ButtonPlay/index.js
+++ app/component/ButtonPlay/index.js
@@ -2,19 +2,31 @@ import React, { Component } from 'react';
 import {
   StyleSheet,
   Text,
-  Button
+  Button,
+  View
 } from 'react-native';
 import { navigate } from 'react-navigation';
 export default class ButtonPlay extends Component {
   render() {
     const { navigate } = this.props.navigation;
     return (
-      <Button
+      <View >
+
+        <Button
         onPress={() => navigate('Player')}
         title="Play"
         color="#841584"
         accessibilityLabel="Learn more about this purple button"
+        marginBottom='10'
+       />
+       <Button
+        onPress={() => navigate('Schedule')}
+        title="Schedule"
+        color="#841584"
+        accessibilityLabel="Learn more about this purple button"
         />
+      </View>
+      
     );
   }
 }
